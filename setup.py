6696b0722ad3c7c6836981285123bc93db0769c1
Shravan4598 <shravankumarpandey825412@gmail.com>
2025-10-08 13:28:36 +0530

    added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements()->list[str]:
+    requirements_list=list[str]=[]
+    return  requirements_list
+
+setup(
+    name="Sensor",
+    version="0.0.1",
+    author="Shravan Kumar Pandey",
+    author_email="shravankumarpandy825412@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+
+)
