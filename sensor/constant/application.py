e61bbe7a053d21cdb118d3db882ddab4cc79c5be
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-02-14 14:48:43 +0530

    Fast API

---
+++ sensor/constant/application.py
@@ -0,0 +1,4 @@
+APP_HOST="0.0.0.0"  #Host IP Address and it will listen on all public IPs to Host Machine
+APP_PORT=8080  # It specifies the port number on which application will listen for incoming HTTP requests
+
+
